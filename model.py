e3da6a34fba599b5bc775222875377c828e5ff7c
Matej Zupančič <matej.zupancic.187@gmail.com>
2020-05-12 15:13:16 +0200

    Dopolnil model.

--- model.py
+++ model.py
@@ -8,33 +8,59 @@ ZMAGA = 'W'
 PORAZ = 'X'
 
 class Igra:
-    def __init__(self):
-        pass
-        self.geslo = 0
-        self.crke = []
+    def __init__(self, geslo, crke=[]):
+        self.geslo = geslo.lower()
+        
+        self.crke = [z.lower() for z in crke]
 
     def napacne_crke(self):
-        pass
+        napacne = [c for c in self.crke if c not in self.geslo]
+        return napacne
 
     def pravilne_crke(self):
-        pass
+        pravilne = [c for c in self.crke if c in self.geslo]
+        return pravilne
 
     def stevilo_napak(self):
-        pass
+        return len(self.napacne_crke())
 
     def zmaga(self):
-        pass
+        for c in self.geslo:
+            if c not in self.crke:
+                return False
+
+        return True
 
     def poraz(self):
-        pass
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
 
     def pravilni_del_gesla(self):
-        pass
+        pravilni_del = ""
+        for c in self.geslo:
+            if c in self.crke:
+                pravilni_del += c
+            else:
+                pravilni_del += "_"
 
     def nepravilni_ugibi(self):
-        pass
+        return " ".join(self.napacne_crke())
 
     def ugibaj(self, crka):
-        pass
+        crka = crka.lower()
 
-    
+        if crka in self.crke:
+            return PONOVLJENA_CRKA
+
+        self.crke.append(crka)
+
+        if crka in self.geslo:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+
